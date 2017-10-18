117f8553c26041605eb242f94f81002cb7431ff3
Justin Livi <silaslivi@gmail.com>
2017-10-17 23:09:25 -0400

    Add main branding page

--- src/components/Pages/index.js
+++ src/components/Pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './Home';
 import Experience from './Experience';
+import Branding from './Branding';
 import { OpenSource } from './OpenSource';
 
 function getPage( route ) {
@@ -8,6 +9,7 @@ function getPage( route ) {
         case 'home': return () => <Home />;
         case 'experience': return () => <Experience />;
         case 'open-source': return () => <OpenSource />;
+        case 'branding': return () => <Branding />;
         default: return () => <div>{route}</div>;
     }
 }
--- src/components/routes.js
+++ src/components/routes.js
@@ -4,6 +4,7 @@ export const routes = [
     'home',
     'experience',
     'open-source',
+    'branding',
     'contact',
 ];
 
