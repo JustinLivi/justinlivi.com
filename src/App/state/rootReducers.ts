7b43fdb5bfddc0b6dea1bfe37a840d5d39810bba
Justin Livi <silaslivi@gmail.com>
2020-12-14 05:09:04 -0500

    Add basic theme toggle

--- src/App/state/rootReducers.ts
+++ src/App/state/rootReducers.ts
@@ -1,13 +1,17 @@
 import { combineReducers } from 'redux';
 import { createBrowserHistory } from 'history';
 import { createReduxHistoryContext } from 'redux-first-history';
-import { reducer } from 'ts-action';
+import { on, reducer } from 'ts-action';
 
 import { headerStateReducer } from 'Header/state/headerReducers';
 import { packageStateReducer } from 'RootPages/reducers/packages';
 import { ColorThemeId } from 'styles/colorThemes/colorThemeTypes';
+import { toggleColorTheme } from 'App/state/actions';
 
-export const themeReducer = reducer(ColorThemeId.light);
+export const themeReducer = reducer(
+  ColorThemeId.light,
+  on(toggleColorTheme, theme => +!theme),
+);
 
 if ('scrollRestoration' in window.history) {
   window.history.scrollRestoration = 'manual';
--- src/styles/interactive.ts
+++ src/styles/interactive.ts
@@ -1,19 +1,23 @@
 import { css } from 'styled-components';
 
-import { color } from 'styles/colorThemes/colorTheme';
+import { color, themeTransition } from 'styles/colorThemes/colorTheme';
 import { ColorToken } from 'styles/colorThemes/colorThemeTypes';
 
 const width = '0.1rem';
 
 export const interactiveStyle = css`
   color: ${color(ColorToken.primary)};
+
+  padding-left: 0;
+  padding-right: 0;
+
   text-decoration: none;
   background: none;
   cursor: pointer;
   -webkit-appearance: none;
   -moz-appearance: none;
 
-  transition: border-bottom-color 300ms;
+  transition: border-bottom-color 300ms linear, ${themeTransition};
   border: 0;
   border-bottom-width: ${width};
   border-bottom-color: ${color(ColorToken.interactiveHiglightInactive)};
@@ -28,12 +32,12 @@ export const interactiveStyle = css`
 
   &:focus,
   &:hover {
-    transition: border-bottom-color 200ms;
+    transition: border-bottom-color 200ms linear, ${themeTransition};
     border-bottom-color: ${color(ColorToken.interactiveHiglightHover)};
   }
 
   &:active {
-    transition: border-bottom-color 100ms;
+    transition: border-bottom-color 100ms linear, ${themeTransition};
     border-bottom-color: ${color(ColorToken.interactiveHiglightActive)};
     outline: none;
   }
