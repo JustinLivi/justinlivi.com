d15e10e169e4437e1acfe9e9181f4be7e10cf0df
Justin Livi <silaslivi@gmail.com>
2020-12-14 21:37:30 -0500

    Fix thumbnail hover behavior

--- src/RootPages/components/thumbnails/BrinkbitThumb.tsx
+++ src/RootPages/components/thumbnails/BrinkbitThumb.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Grid } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 import { color } from 'styles/colorThemes/colorTheme';
 import { ColorToken } from 'styles/colorThemes/colorThemeTypes';
+import {
+  interactiveHoverStyle,
+  interactiveFocusStyle,
+  interactiveActiveStyle,
+  interactiveBaseStyle,
+  interactiveBaseTiming,
+  interactiveHoverTiming,
+  interactiveFocusTiming,
+  interactiveActiveTiming,
+} from 'styles/interactive';
 
 const Polygon = styled.polygon``;
 
@@ -13,24 +25,68 @@ const ThumbSpan = styled.span`
   padding-bottom: 10%;
   padding-left: 40%;
   padding-right: 40%;
+  margin-bottom: 1em;
   background-color: ${color(ColorToken.greyPrimary)};
 
   ${Polygon} {
     fill: ${color(ColorToken.greySecondary)};
-    transition: fill 0.4s ease;
+    transition: fill ${interactiveBaseTiming};
   }
+`;
 
-  &:hover {
+const StyledParagraph = styled.p`
+  display: contents;
+  ${interactiveBaseStyle}
+`;
+
+const StyledLink = styled(Link)`
+  &:focus {
+    ${StyledParagraph} {
+      ${interactiveFocusStyle}
+    }
+
     ${Polygon} {
       fill: ${color(ColorToken.brinkbitBlue)};
+      transition: fill ${interactiveFocusTiming};
+    }
+  }
+
+  &:active {
+    ${StyledParagraph} {
+      ${interactiveActiveStyle}
+    }
+
+    ${Polygon} {
+      fill: ${color(ColorToken.brinkbitBlue)};
+      transition: fill ${interactiveActiveTiming};
+    }
+  }
+`;
+
+const StyledSpan = styled.span`
+  &:hover {
+    ${StyledParagraph} {
+      ${interactiveHoverStyle}
+    }
+
+    ${Polygon} {
+      fill: ${color(ColorToken.brinkbitBlue)};
+      transition: fill ${interactiveHoverTiming};
     }
   }
 `;
 
 export const BrinkbitThumb: React.FunctionComponent = () => (
-  <ThumbSpan role="link">
-    <svg viewBox="0 -5.69 53.81 83.12">
-      <Polygon points="17.94 17.94 17.94 0 0 0 0 71.75 35.88 71.75 35.88 53.81 17.94 53.81 17.94 35.88 35.88 35.88 35.88 53.81 53.81 53.81 53.81 17.94 17.94 17.94" />
-    </svg>
-  </ThumbSpan>
+  <Grid item={true} xs={12} sm={6} lg={3}>
+    <StyledLink to="design/brinkbit">
+      <StyledSpan>
+        <ThumbSpan role="link">
+          <svg viewBox="0 -5.69 53.81 83.12">
+            <Polygon points="17.94 17.94 17.94 0 0 0 0 71.75 35.88 71.75 35.88 53.81 17.94 53.81 17.94 35.88 35.88 35.88 35.88 53.81 53.81 53.81 53.81 17.94 17.94 17.94" />
+          </svg>
+        </ThumbSpan>
+        <StyledParagraph>brinkbit</StyledParagraph>
+      </StyledSpan>
+    </StyledLink>
+  </Grid>
 );
--- src/RootPages/components/thumbnails/WatercolorSedimentThumb.tsx
+++ src/RootPages/components/thumbnails/WatercolorSedimentThumb.tsx
@@ -1,11 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { Grid } from '@material-ui/core';
 
 import { ThumbnailContainer } from 'SharedComponents/DesignDetailsElements';
+import {
+  interactiveBaseStyle,
+  interactiveFocusStyle,
+  interactiveActiveStyle,
+  interactiveHoverStyle,
+} from 'styles/interactive';
 
 import watercolorSedimentThumb from './watercolorSedimentThumb.jpg';
 
+const StyledParagraph = styled.p`
+  display: inline-block;
+  border-bottom: solid;
+  ${interactiveBaseStyle}
+`;
+
+const StyledLink = styled(Link)`
+  &:focus {
+    ${StyledParagraph} {
+      ${interactiveFocusStyle}
+    }
+  }
+
+  &:active {
+    ${StyledParagraph} {
+      ${interactiveActiveStyle}
+    }
+  }
+`;
+
+const StyledSpan = styled.span`
+  display: block;
+  &:hover {
+    ${StyledParagraph} {
+      ${interactiveHoverStyle}
+    }
+  }
+`;
+
 export const WatercolorSedimentThumb: React.FunctionComponent = () => (
-  <ThumbnailContainer>
-    <img src={watercolorSedimentThumb} alt="watercolor sediment" />
-  </ThumbnailContainer>
+  <Grid item={true} xs={12} sm={6} lg={3}>
+    <StyledLink to="generative-art/watercolor-sediment">
+      <StyledSpan>
+        <ThumbnailContainer src={watercolorSedimentThumb} alt="watercolor sediment" />
+        <StyledParagraph>watercolor sediment</StyledParagraph>
+      </StyledSpan>
+    </StyledLink>
+  </Grid>
 );
