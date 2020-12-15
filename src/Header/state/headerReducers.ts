3642f8bdcee6ab4db3b0313c30c482df2b56589c
Justin Livi <silaslivi@gmail.com>
2020-12-14 22:47:33 -0500

    Improve navigation focus management

--- src/Header/state/headerReducers.ts
+++ src/Header/state/headerReducers.ts
@@ -1,9 +1,10 @@
 import { routerLocationChange } from 'App/state/actions';
-import { breadcrumbsBlur, breadcrumbsSelect } from 'Header/state/headerActions';
+import { breadcrumbsBlur, breadcrumbsSelect, menuItemBlur, menuItemFocus } from 'Header/state/headerActions';
 import { on, reducer } from 'ts-action';
 
 export const headerStateReducer = reducer(
   false,
-  on(breadcrumbsBlur, routerLocationChange, () => false),
+  on(breadcrumbsBlur, menuItemBlur, routerLocationChange, () => false),
   on(breadcrumbsSelect, state => !state),
+  on(menuItemFocus, () => true),
 );
