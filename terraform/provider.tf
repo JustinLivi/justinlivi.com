ea7211deae02127bc96039e7db52e884a0a11bca
Justin Livi <silaslivi@gmail.com>
2020-12-15 06:35:54 -0500

    Upgrade terraform version

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  version    = "~> 2.48"
   region     = var.region
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,4 +2,13 @@ terraform {
   backend "s3" {
     key = "justin-livi"
   }
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 2.48"
+    }
+  }
+  
+  required_version = ">= 0.13"
 }
